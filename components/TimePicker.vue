a2b1c164e2ccd5fd34f0ac75e4dd0cf3cc9379ce
LeeHanYeong <dev@azelf.com>
2018-02-18 20:28:59 +0900

    First commit

---
+++ components/TimePicker.vue
@@ -0,0 +1,23 @@
+<template>
+  <input id="timepicker" v-model="value" type="text">
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  props: ['value'],
+  mounted () {
+    // 이거는 잘 됨
+    console.log($('#timepicker'))
+    console.log($(this.$el))
+
+    // 이거는 안 됨
+    // pickadate js모듈을 어떻게 불러와야 하는지 모르겠음
+    // npm install pickadate로 설치 가능
+    
+    // $('#timepicker').pickatime()
+    $(this.$el).pickatime()
+  }
+}
+</script>
